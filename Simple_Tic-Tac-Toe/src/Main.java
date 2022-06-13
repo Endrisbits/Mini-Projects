4426ab596242517f944d08576552d6e20e20b932
Endrisbits <mendri94@gmail.com>
2022-06-14 01:27:56 +0200

    Did some cleaning up to make the code more readable.

--- Simple_Tic-Tac-Toe/src/Main.java
+++ Simple_Tic-Tac-Toe/src/Main.java
@@ -1,20 +1,19 @@
-package tictactoe;
-
 import java.util.Scanner;
 
 public class Main {
     private static Scanner m_Scanner = new Scanner(System.in);
-    private enum flowState{}
+
     public static void main(String[] args) {
         //Prepare for new game
-        TicTacToe.reset();
-        TicTacToe.printField();
+        TicTacToe game = new TicTacToe();
+        game.reset();
+        game.printField();
 
         //Ask user for input
-        while(!TicTacToe.gameEnded()) {
+        while(!game.gameEnded()) {
             boolean invalidInput = true;
             while (invalidInput) {//handle user input (2 int must be entered, representing the coordinates for the move
-                System.out.print(TicTacToe.m_currentTurn+"Enter the coordinates: ");
+                System.out.print(game.getCurrentPlayerTurn() + " Enter the coordinates: ");
                 if (!m_Scanner.hasNextInt()) System.out.println("You should enter numbers!");
                 else {
                     int x = m_Scanner.nextInt();
@@ -25,9 +24,9 @@ public class Main {
                         if ((x < 1 || x > 3) || (y < 1 || y > 3)) {
                             System.out.println("Coordinates should be from 1 to 3!");
                         } else { //input withing bounds. Check for empty cell.
-                            if (TicTacToe.validMove(x, y)) {
-                                TicTacToe.printField();
-                                TicTacToe.analyzeGameState();
+                            if (game.validMove(x, y)) {
+                                game.printField();
+                                game.analyzeGameState();
                                 invalidInput = false;
                             } else {
                                 System.out.println("This cell is occupied! Choose another one!");
@@ -37,6 +36,6 @@ public class Main {
                 }
             }
         }
-        TicTacToe.printState();
+        game.printState();
     }
 }
--- Simple_Tic-Tac-Toe/src/TicTacToe.java
+++ Simple_Tic-Tac-Toe/src/TicTacToe.java
@@ -1,61 +1,12 @@
-package tictactoe;
-
 class TicTacToe {
 
-    public static void reset() {
+    public void reset() {
         setFieldFromString("_________");
         analyzeGameState();
         m_currentTurn=Turn.PLAYER_X;
     }
 
-    public static boolean gameEnded() {
-        return(m_gameState==GameState.DRAW ||
-               m_gameState==GameState.X_WIN ||
-               m_gameState==GameState.O_WIN);
-    }
-
-    private enum FieldValues {
-       X('X'),
-       O('O'),
-       E('_');
-
-       public final char label;
-
-       FieldValues(char c) {
-           this.label = c;
-       }
-
-       public static FieldValues valueFromChar(char c) {
-           switch (c) {
-               case 'X': return FieldValues.X;
-               case 'O': return FieldValues.O;
-               case '_': return FieldValues.E;
-           }
-           return FieldValues.E;
-       }
-   }
-    private enum GameState{NOT_FINISHED, DRAW, X_WIN, O_WIN, IMPOSSIBLE}
-
-    private static TicTacToe.FieldValues[] gameField = new FieldValues[] {
-            FieldValues.E, FieldValues.E, FieldValues.E,
-            FieldValues.E, FieldValues.E, FieldValues.E,
-            FieldValues.E, FieldValues.E, FieldValues.E,
-    };
-
-    public enum Turn {PLAYER_X,PLAYER_O}
-    public static Turn m_currentTurn = Turn.PLAYER_X;
-    public static GameState m_gameState = GameState.NOT_FINISHED;
-
-    public static void setFieldFromString(String cells) {
-        if(cells.length()<9 || cells.length()>9) {}
-        else {
-            for (int i = 0; i < 9; i++) {
-                    gameField[i] = FieldValues.valueFromChar(cells.charAt(i));
-                }
-        }
-    }
-
-    public static void printField(){
+    public void printField() {
         System.out.println("---------");
         for (int i=0; i<3; i++){
             System.out.printf("| %c %c %c |%n",gameField[i*3].label, gameField[i*3+1].label, gameField[i*3+2].label);
@@ -63,7 +14,17 @@ class TicTacToe {
         System.out.println("---------");
     }
 
-    public static void analyzeGameState(){
+    public boolean gameEnded() {
+        return(m_gameState==GameState.DRAW ||
+                m_gameState==GameState.X_WIN ||
+                m_gameState==GameState.O_WIN);
+    }
+
+    public Turn getCurrentPlayerTurn() {
+        return m_currentTurn;
+    }
+
+    public void analyzeGameState(){
         int counterX = 0;
         int counterO = 0;
         int counterE = 0;
@@ -80,30 +41,16 @@ class TicTacToe {
         /*Impossible Cases First*/
         if(Math.abs(counterX-counterO)>1) m_gameState = GameState.IMPOSSIBLE; //Too many Xs or Os
         else if(xWon && oWon) m_gameState=GameState.IMPOSSIBLE; //Both Players Won
-        /*Draw*/
+            /*Draw*/
         else if(!xWon && !oWon && counterE==0) m_gameState=GameState.DRAW;
-        /*Either Player Won*/
+            /*Either Player Won*/
         else if(xWon) m_gameState = GameState.X_WIN;
         else if(oWon) m_gameState = GameState.O_WIN;
-        /*Game not finished yet*/
+            /*Game not finished yet*/
         else m_gameState = GameState.NOT_FINISHED;
     }
 
-    public static boolean checkWin(FieldValues v) {
-       for(int i = 0; i<3; i++) { //Filled one row?
-           if(gameField[i*3] == v && gameField[i*3]==gameField[i*3+1] && gameField[i*3+1]==gameField[i*3+2]) return true;
-       }
-       for(int i=0; i<3; i++) { //Filled one column?
-           if(gameField[i] == v && gameField[i] == gameField[i+3] && gameField[i+3]==gameField[i+6]) return true;
-       }
-       if(gameField[4]==v) { //Filled either diagonal?
-           if(gameField[0]==v && gameField[0]==gameField[8]) return true;
-           else if(gameField[2]==v && gameField[2]==gameField[6]) return true;
-       }
-       return false; //none of the win conditions were met
-    }
-
-    public static void printState() {
+    public void printState() {
         switch (m_gameState) {
             case DRAW: System.out.println("Draw"); break;
             case X_WIN: System.out.println("X wins"); break;
@@ -115,7 +62,7 @@ class TicTacToe {
         }
     }
 
-    public static boolean validMove(int x, int y) { //inputs x and y from (1-3)
+    public boolean validMove(int x, int y) { //inputs x and y from (1-3)
         int i=x-1; int j=y-1;
         if(gameField[i*3+j]==FieldValues.E) {//is the field empty at (i,j)?
             switch (m_currentTurn) {
@@ -132,4 +79,62 @@ class TicTacToe {
         }
         else {return false;}
     }
+
+    private enum FieldValues {
+        X('X'),
+        O('O'),
+        E('_');
+
+        public final char label;
+
+        FieldValues(char c) {
+            this.label = c;
+        }
+
+        public static FieldValues valueFromChar(char c) {
+            switch (c) {
+                case 'X': return FieldValues.X;
+                case 'O': return FieldValues.O;
+                case '_': return FieldValues.E;
+            }
+            return FieldValues.E;
+        }
+    }
+
+    private enum GameState{NOT_FINISHED, DRAW, X_WIN, O_WIN, IMPOSSIBLE}
+
+    private TicTacToe.FieldValues[] gameField = new FieldValues[] {
+            FieldValues.E, FieldValues.E, FieldValues.E,
+            FieldValues.E, FieldValues.E, FieldValues.E,
+            FieldValues.E, FieldValues.E, FieldValues.E
+    };
+
+    public enum Turn {PLAYER_X,PLAYER_O};
+
+    private GameState m_gameState = GameState.NOT_FINISHED;
+
+    private void setFieldFromString(String cells) {
+        if(cells.length()<9 || cells.length()>9) {}
+        else {
+            for (int i = 0; i < 9; i++) {
+                gameField[i] = FieldValues.valueFromChar(cells.charAt(i));
+            }
+        }
+    }
+
+    private  boolean checkWin(FieldValues v) {
+        for(int i = 0; i<3; i++) { //Filled one row?
+            if(gameField[i*3] == v && gameField[i*3]==gameField[i*3+1] && gameField[i*3+1]==gameField[i*3+2]) return true;
+        }
+        for(int i=0; i<3; i++) { //Filled one column?
+            if(gameField[i] == v && gameField[i] == gameField[i+3] && gameField[i+3]==gameField[i+6]) return true;
+        }
+        if(gameField[4]==v) { //Filled either diagonal?
+            if(gameField[0]==v && gameField[0]==gameField[8]) return true;
+            else if(gameField[2]==v && gameField[2]==gameField[6]) return true;
+        }
+        return false; //none of the win conditions were met
+    }
+
+    private  Turn m_currentTurn = Turn.PLAYER_X;
 }
